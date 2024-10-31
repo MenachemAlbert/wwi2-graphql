aef3c8c6aebf5311e702ec4e73593be16369c04e
menachem <ma0556703449@gmail.com>
2024-10-31 12:17:36 +0200

    end query by data renge

--- app/gql/query.py
+++ app/gql/query.py
@@ -1,12 +1,21 @@
-from graphene import ObjectType, Field, Int
+from graphene import ObjectType, Field, Int, List, Date, Argument
 
 from app.gql.types.mission_type import MissionType
-from app.repository.mission_repository import get_mission_by_id
+from app.repository.mission_repository import get_mission_by_id, get_mission_by_date_range
 
 
 class Query(ObjectType):
     mission_by_id = Field(MissionType, mission_id=Int())
+    mission_by_date_range = List(
+        MissionType,
+        start_date=Argument(Date, required=True),
+        end_date=Argument(Date, required=True)
+    )
 
     @staticmethod
     def resolve_mission_by_id(root, info, mission_id):
         return get_mission_by_id(mission_id).unwrap()
+
+    @staticmethod
+    def resolve_mission_by_date_range(root, info, start_date, end_date):
+        return get_mission_by_date_range(start_date, end_date)
--- app/repository/mission_repository.py
+++ app/repository/mission_repository.py
@@ -1,4 +1,8 @@
+from datetime import date
+from typing import List
+
 from returns.maybe import Maybe
+from sqlalchemy import and_
 
 from app.db.database import session_maker
 from app.db.models import Mission
@@ -7,3 +11,8 @@ from app.db.models import Mission
 def get_mission_by_id(mission_id: int) -> Maybe[Mission]:
     with session_maker() as session:
         return Maybe.from_optional(session.query(Mission).filter_by(mission_id=mission_id).first())
+
+
+def get_mission_by_date_range(start_date: date, end_date: date) -> List[Mission]:
+    with session_maker() as session:
+        return session.query(Mission).filter(Mission.mission_date.between(start_date, end_date)).all()
